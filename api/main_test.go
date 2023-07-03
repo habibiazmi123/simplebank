24aa7fc0557754e3ee3fc2225adf43066947deea
Muhamad Habibi Azmi <m.habibiazmi@gmail.com>
2023-07-03 14:00:11 +0700

    add paseto

--- api/main_test.go
+++ api/main_test.go
@@ -3,9 +3,25 @@ package api
 import (
 	"os"
 	"testing"
+	"time"
 
 	"github.com/gin-gonic/gin"
+	db "github.com/habibiazmi123/simplebank/db/sqlc"
+	"github.com/habibiazmi123/simplebank/util"
+	"github.com/stretchr/testify/require"
 )
+
+func newTestServer(t *testing.T, store db.Store) *Server {
+	config := util.Config{
+		TokenSymmetricKey:   util.RandomString(12),
+		AccessTokenDuration: time.Minute,
+	}
+
+	server, err := NewServer(config, store)
+	require.NoError(t, err)
+
+	return server
+}
 
 func TestMain(m *testing.M) {
 	gin.SetMode(gin.TestMode)
