c8b6a665bb126c38c4cdb2dd1951804ffd9eef5d
Muhamad Habibi Azmi <m.habibiazmi@gmail.com>
2023-09-04 14:50:58 +0700

    make gmail sender & skip all test

---
+++ mail/sender_test.go
@@ -0,0 +1,31 @@
+package mail
+
+import (
+	"testing"
+
+	"github.com/habibiazmi123/simplebank/util"
+	"github.com/stretchr/testify/require"
+)
+
+func TestSendEmailWithGmail(t *testing.T) {
+	if testing.Short() {
+		t.Skip()
+	}
+
+	config, err := util.LoadConfig("..")
+	require.NoError(t, err)
+
+	sender := NewGmailSender(config.EmailSenderName, config.EmailSenderAddress, config.EmailSenderPassword)
+
+	subject := "A test email"
+	content := `
+	<h1>Hello ini test from cumi</h1>
+	<p>Seru belajar GOLANG!!</p>
+	`
+
+	to := []string{"m.habibiazmi@gmail.com"}
+	attachFiles := []string{"../README.md"}
+
+	err = sender.SendEmail(subject, content, to, nil, nil, attachFiles)
+	require.NoError(t, err)
+}
